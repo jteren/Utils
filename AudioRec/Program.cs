d3a40663aab4d421fe6d0fb0571d86207b5d4a9e
Jan Teren <terenjan@gmail.com>
2025-11-09 19:07:37 +0000

    Added mutex to only allow one instance

--- AudioRec/Program.cs
+++ AudioRec/Program.cs
@@ -2,16 +2,34 @@ namespace AudioRec
 {
     internal static class Program
     {
+
+        private static Mutex mutex;
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            bool createdNew;
+            mutex = new Mutex(true, "AudioRecAppMutex", out createdNew);
+
+            if (!createdNew)
+            {
+                // Another instance is already running
+                MessageBox.Show("AudioRec is already running.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
+
+            // Release the mutex when the app exits
+            mutex.ReleaseMutex();
+
         }
     }
 }
